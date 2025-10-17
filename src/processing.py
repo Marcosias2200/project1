60bfd5288d3008d9165fdecb8fa0772089ccaa69
Marcosias <pol.poin@mail.ru>
2025-10-17 10:18:31 +0300

    Исправил функию sort_by_date и добавил docstring на каждую функцию

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,15 @@
 from datetime import datetime
+
 def filter_by_state(data, state='EXECUTED'):
+    """ Фильтрует список транзакций по состоянию."""
     return [item for item in data if item.get('state') == state]
 
 
 def sort_by_date(data, descending=True):
-    return sorted(data, key=lambda item: datetime.strptime(item['date'], '%Y-%m-%d'), reverse=descending)
+    """param1 (тип): Описание первого параметра.
+    - param2 (тип): Описание второго параметра."""
+    return sorted(
+        data,
+        key=lambda item: datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%S.%f'),
+        reverse=descending
+    )
