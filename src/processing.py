87239c8ff2e70c86501033e13f9c884cccbc1812
Marcosias <pol.poin@mail.ru>
2025-10-20 12:55:03 +0300

    Добавлен более побдробный docstring

--- src/processing.py
+++ src/processing.py
@@ -6,8 +6,7 @@ def filter_by_state(data, state='EXECUTED'):
 
 
 def sort_by_date(data, descending=True):
-    """param1 (тип): Описание первого параметра.
-    - param2 (тип): Описание второго параметра."""
+    """Функция сортирует список словарей data по дате."""
     return sorted(
         data,
         key=lambda item: datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%S.%f'),
