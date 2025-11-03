49c56a17daab753eee525208651ed8c5f4ec95d3
Marcosias <pol.poin@mail.ru>
2025-11-03 19:34:29 +0300

    Тесть для функции valid_car_number Проверка на боньше больше букву пустая строка (ИЗМЕНИЛ КОД)

--- tests/tests_hom_task.py
+++ tests/tests_hom_task.py
@@ -3,28 +3,24 @@ import pytest
 from src.masks import validate_card_number
 
 
-def test_masks():
-    assert validate_card_number
-
-
-def valid_card_number():
+@pytest.fixture
+def valid_card():
     return "1234567812345678"
 
-@pytest.fixture
-def invalid_card_numbers():
-        return [
+# Фикстура для списка невалидных номеров карт
+@pytest.fixture(params=[
+    "123456781234567",   # Меньше 16 цифр
+    "12345678123456789", # Больше 16 цифр
+    "123456781234567a",  # Содержит буквы
+    "1234-5678-1234-5678", # Формат не соответствует
+    "",                   # Пустая строка
+])
+def invalid_card(request):
+    return request.param
 
-                "123456781234567",  # Меньше 16 цифр
-                "12345678123456789",  # Больше 16 цифр
-                "123456781234567a",  # Содержит буквы
-                "1234-5678-1234-5678",  # Формат не соответствует
-                "",  # Пустая строка
+def test_valid_card(valid_card):
+    assert validate_card_number(valid_card) is True
 
-        ]
-def test_validate_card_number_with_valid_input(valid_card_numbers):
-    assert validate_card_number(valid_card_numbers) is True
-
-def test_validate_card_number_with_invalid_input(invalid_card_numbers):
-    for card_number in invalid_card_numbers:
-        assert validate_card_number(card_number) is False
+def test_invalid_cards(invalid_card):
+    assert validate_card_number(invalid_card) is False
 
