0298c3ebf5c604517cef76ab2eaf060c202d2e41
Marcosias <pol.poin@mail.ru>
2025-11-10 12:29:21 +0300

    написал все тесты на processing.py проверяет список и его сортировку

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,25 @@ def mask_number():
 @pytest.fixture
 def get_mask():
     return "**4305"
+
+
+@pytest.fixture
+def transactions():
+    return [
+        {'id': 1, 'amount': 100, 'state': 'EXECUTED'},
+        {'id': 2, 'amount': 250, 'state': 'CANCELED'},
+        {'id': 3, 'amount': 150, 'state': 'EXECUTED'},
+        {'id': 4, 'amount': 300, 'state': 'PENDING'},
+        {'id': 5, 'amount': 50, 'state': 'EXECUTED'},
+    ]
+
+
+@pytest.fixture
+def sort_bate():
+    return [
+        {'id': 1, 'amount': 100, 'date': '2023-09-01T10:00:00.000000'},
+        {'id': 2, 'amount': 250, 'date': '2023-09-03T12:00:00.000000'},
+        {'id': 3, 'amount': 150, 'date': '2023-09-02T14:00:00.000000'},
+        {'id': 4, 'amount': 300, 'date': '2023-09-01T09:00:00.000000'},
+        {'id': 5, 'amount': 50, 'date': '2023-09-03T15:00:00.000000'},
+    ]
